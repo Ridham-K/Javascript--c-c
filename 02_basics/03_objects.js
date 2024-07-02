012594f904b06374bcf5eb7ada0dbe0da942f814
Ridham-K <ridhamkansara26@gmail.com>
2024-07-02 11:50:07 +0530

    objects discussion

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Ridham",
+    "full name": "Ridham Kansara",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Ahemdabad",
+    email: "rk@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "ridham@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "ridham@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+console.log(JsUser);
