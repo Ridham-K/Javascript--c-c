746cbbe99602aca3d32e50421ff633e516387a11
Ridham-K <ridhamkansara26@gmail.com>
2024-07-04 15:27:47 +0530

    filter in JS

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -37,9 +37,12 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-  let userBooks = books.filter( (bk) => bk.genre === 'History')
+//   let userBooks = books.filter( (bk) => bk.genre === 'History')
 
-  userBooks = books.filter( (bk) => { 
-    return bk.publish >= 1995 && bk.genre === "History"
-})
+//   userBooks = books.filter( (bk) => { 
+//     return bk.publish >= 1995 && bk.genre === "History"
+// })
+//   console.log(userBooks);
+  
+  let userBooks = books.filter( (bk) => bk.edition >= 2007)
   console.log(userBooks);
