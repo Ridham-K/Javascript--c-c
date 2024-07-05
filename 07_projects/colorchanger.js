8ea5223fd6137269ea3f7f10156a4d0e1be2a684
Ridham-K <ridhamkansara26@gmail.com>
2024-07-05 11:41:28 +0530

    project 1

---
+++ 07_projects/colorchanger.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+// console.log(buttons)
+
+buttons.forEach(function (button) {
+  // console.log(c);
+  button.addEventListener('click', function (e) {
+    console.log(e.target.id);
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = e.target.id;
+        break;
+
+      case 'white':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'blue':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'yellow':
+        body.style.backgroundColor = e.target.id;
+        break;
+      case 'red':
+        body.style.backgroundColor = e.target.id;  
+      
+    }
+  });
+});
